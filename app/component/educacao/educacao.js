74b30dc128e601fb91312bc0ce7989b29eb7f52e
tjulioh <thiagojulio20@gmail.com>
2021-06-15 09:47:19 -0300

    Ajuste da requisição.

--- app/component/educacao/educacao.js
+++ app/component/educacao/educacao.js
@@ -1,17 +1,21 @@
 app.controller('educacao', function($scope, $http) {
-
-    $scope.items = []
-  
-    $http({
-       method: 'GET',
-       url: 'http://127.0.0.1:8080/api/pais/',
-       headers: {'authorization':'Basic YWRtaW46YWRtaW4'}
-    })
-      .then(function successCallback(response) {
-          alert("Succesfully connected to the API");
-          $scope.items = data;
-      }, function errorCallback(response) {
-          alert("Error connecting to API");
-      }); 
-  
-  });
+    $scope.getItems = () => {
+        $scope.items = []
+        $http({
+            method: 'GET',
+            url: 'http://127.0.0.1:8080/api/pais/',
+            headers: {'authorization':'Basic YWRtaW46YWRtaW4'}
+        })
+        .then(
+            function successCallback(resultado) {
+                $scope.items = resultado.data;
+            },
+            function errorCallback(resultado) {
+                console.log(resultado)
+                alert("Houve um problema de conexão!");
+            }
+        );
+    }
+    console.log('CHAMA');
+    $scope.getItems();
+});
