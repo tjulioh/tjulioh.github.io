9f32b6fff4feb38f0a27ac8fca4c7b31b8d2a001
Thiago Julio <thiagojulio20@gmail.com>
2021-06-16 00:06:36 -0300

    Ajustado Integração.

--- app/component/educacao/educacao.js
+++ app/component/educacao/educacao.js
@@ -1,21 +1,24 @@
-app.controller('educacao', function($scope, $http) {
-    $scope.getItems = () => {
-        $scope.items = []
-        $http({
-            method: 'GET',
-            url: 'http://127.0.0.1:8080/api/pais/',
-            headers: {'authorization':'Basic YWRtaW46YWRtaW4'}
-        })
-        .then(
-            function successCallback(resultado) {
-                $scope.items = resultado.data;
-            },
-            function errorCallback(resultado) {
-                console.log(resultado)
-                alert("Houve um problema de conexão!");
-            }
-        );
+app.controller('educacao', function($scope, $http) {                        
+    $scope.api = 'pais'
+    $scope.items = []
+    $scope.getItem = () => {                
+        if($scope.api){            
+            $http({
+                method: 'GET',
+                url: 'http://127.0.0.1:8080/api/'+$scope.api+'/',
+                headers: {'authorization':'Basic YWRtaW46YWRtaW4'}
+            })
+            .then(
+                function successCallback(resultado) {
+                    console.log(resultado.data)
+                    $scope.items = resultado.data;
+                },
+                function errorCallback(resultado) {
+                    $scope.items = []
+                    console.log(resultado)
+                }
+            );
+        }
     }
-    console.log('CHAMA');
-    $scope.getItems();
+    $scope.getItem();
 });
