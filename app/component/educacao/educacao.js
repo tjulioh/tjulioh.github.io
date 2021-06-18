4927ed233f5662e391ffb80b5af00423afd1500a
Thiago Julio <thiagojulio20@gmail.com>
2021-06-18 00:08:06 -0300

    Atualização de Segurança.

--- app/component/educacao/educacao.js
+++ app/component/educacao/educacao.js
@@ -6,7 +6,7 @@ app.controller('educacao', function($scope, $http) {
             $http({
                 method: 'GET',
                 url: 'http://ec2-54-233-198-13.sa-east-1.compute.amazonaws.com:8080/api/'+$scope.api+'/',
-                headers: {'authorization':'Basic ZEdwMWJHbHZhQzFoWkcxcGJn'}
+                headers: {'authorization':'Basic YWRtaW46ZEdwMWJHbHZhQzFoWkcxcGJn'}
             })
             .then(
                 function successCallback(resultado) {
