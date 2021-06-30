04cac639282d431db97e871a31eaeeb75ddfd48d
Thiago Julio <thiagojulio20@gmail.com>
2021-06-29 22:58:45 -0300

    Adição de OAuth2

--- app/controller/EducacaoController.js
+++ app/controller/EducacaoController.js
@@ -1,24 +1,49 @@
-app.controller('EducacaoController', function($scope, $http) {                        
-    $scope.api = 'pais'
-    $scope.items = []
-    $scope.getItem = () => {                
-        if($scope.api){            
-            $http({
-                method: 'GET',
-                url: 'https://api.tjulioh.dev/'+$scope.api+'/',
-                headers: {'Authorization':'Basic YWRtaW46ZEdwMWJHbHZhQzFoWkcxcGJn'}
-            })
+app.controller('EducacaoController', function($scope, $http, $document) {
+    $scope.autorizacao = ""
+    $scope.endereco = 'pais'
+    $scope.resultado = []
+    $scope.autorizar = function(){
+        $http({
+            method: 'GET',
+            url: 'https://api.tjulioh.dev/oauth/token?grant_type=password&username=' + $document[0].getElementById('usuario').placeholder + '&password=' + $document[0].getElementById('senha').placeholder,
+            headers: {'Authorization':'Basic ' + btoa('client-id:secret-id')}
+        })
             .then(
                 function successCallback(resultado) {
                     console.log(resultado.data)
-                    $scope.items = resultado.data;
+                    $scope.autorizacao = resultado.data['access_token'];
                 },
                 function errorCallback(resultado) {
-                    $scope.items = []
+                    $scope.resultado = []
                     console.log(resultado)
                 }
             );
+    }
+    $scope.consultar = function() {
+        $scope.autorizar();
+        if ($scope.endereco) {
+            if ($scope.autorizacao) {
+                $http({
+                    method: 'GET',
+                    url: 'https://api.tjulioh.dev/' + $scope.endereco + '/',
+                    headers: {'Authorization': 'Basic ' + $scope.autorizacao}
+                })
+                    .then(
+                        function successCallback(resultado) {
+                            console.log(resultado.data)
+                            $scope.resultado = resultado.data;
+                        },
+                        function errorCallback(resultado) {
+                            $scope.resultado = []
+                            console.log(resultado)
+                        }
+                    );
+            } else {
+                console.log("Sem Autorização")
+            }
+        } else {
+            console.log("Sem Endereço")
         }
     }
-    $scope.getItem();
+    $scope.consultar();
 });
