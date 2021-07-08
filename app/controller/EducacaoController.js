63fc47b76482f6f636e0fcd759a6c2d8a61ca215
Thiago Julio <thiagojulio20@gmail.com>
2021-07-07 21:02:56 -0300

    Adição de OAuth2

--- app/controller/EducacaoController.js
+++ app/controller/EducacaoController.js
@@ -4,31 +4,30 @@ app.controller('EducacaoController', function($scope, $http, $document) {
     $scope.acesso = {usuario:$document[0].getElementById('usuario').placeholder,senha:$document[0].getElementById('senha').placeholder}
 
     $scope.autenticar = function(){
-        if($scope.autorizacao == null){
-            $http({
-                method: 'GET',
-                url: 'https://api.tjulioh.dev/oauth/token?grant_type=password&username=' + $scope.acesso.usuario + '&password=' + $scope.acesso.senha,
-                headers: {
-                    'Access-Control-Allow-Origin':'*',
-                    'Content-Type': 'application/json',
-                    'Authorization':'Basic ' + btoa('educacao:' + btoa('tjulioh-educacao'))
-                }
-            }).then(
-                function successCallback(resultado) {
-                    window.localStorage.setItem('acesso_autorizacao', resultado.data.access_token);
-                    window.localStorage.setItem('acesso_atualizacao', resultado.data.refresh_token);
-                    $scope.autorizacao = resultado.data.access_token;
-                },
-                function errorCallback(resultado) {
-                    $scope.resultado = []
-                    console.log(resultado)
-                }
-            );
-        }
+        $http({
+            method: 'GET',
+            url: 'https://api.tjulioh.dev/oauth/token?grant_type=password&username=' + $scope.acesso.usuario + '&password=' + $scope.acesso.senha,
+            headers: {
+                'Access-Control-Allow-Origin':'*',
+                'Content-Type': 'application/json',
+                'Authorization':'Basic ' + btoa('educacao:' + btoa('tjulioh-educacao'))
+            }
+        }).then(
+            function successCallback(resultado) {
+                window.localStorage.setItem('acesso_autorizacao', resultado.data.access_token);
+                window.localStorage.setItem('acesso_atualizacao', resultado.data.refresh_token);
+                $scope.autorizacao = resultado.data.access_token;
+            },
+            function errorCallback(resultado) {
+                $scope.resultado = []
+                console.log(resultado)
+            }
+        );
     }
 
     $scope.consultar = function() {
-        $scope.autenticar();
+        $scope.autenticar()
+        console.log($scope.autorizacao)
         if ($scope.endereco) {
             if ($scope.autorizacao) {
                 $http({
