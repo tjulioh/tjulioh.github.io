f661701fe1caaf2e6e46700c95abd4a6ee2b2d55
Thiago Julio <thiagojulio20@gmail.com>
2021-07-10 18:20:21 -0300

    Atualização na estruturação

--- app/controller/InicioController.js
+++ app/controller/InicioController.js
@@ -1,3 +1,10 @@
-app.controller('InicioController', ['$scope', function($scope){
+app.controller('InicioController', function($scope, $document) {
     $scope.mensagem = 'Seja bem vindo, pegue um café e leia com calma alguns projetos legais!';
-}]); 
+    if ($document[0].getElementById('dica')) {
+        if(!bootstrap.Tooltip.getInstance($document[0].getElementById('dica'))){
+            new bootstrap.Tooltip($document[0].getElementById('dica'))
+        }else{
+            bootstrap.Tooltip.getInstance($document[0].getElementById('dica')).update()
+        }
+    }
+});
